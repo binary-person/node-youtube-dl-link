be52e61f27a7fd058f86392d23d931993b51d541
Simon Cheng <simoncheng559@gmail.com>
2019-01-31 02:27:46 +0000

    First Deploy

---
+++ README.md
@@ -0,0 +1,29 @@
+## Node Youtube-dl Link API ##
+##### A simple project by Simon Cheng. Special thanks to the awesome library [youtube-dl driver for node](https://github.com/przemyslawpluta/node-youtube-dl) #####
+Hi everyone! This is a program that **only gets high quality videos.** It's only because it's more convenient for myself and others who want use it.
+
+Anyway, to run the program, be sure you have a node version higher than 8.
+```
+node version
+```
+If not, than install it from the official website, or use a really neat tool: [Node Version Manager](https://github.com/creationix/nvm)
+
+To install all neccessary libraries:
+```
+npm install
+```
+And finally to run the program:
+```
+node index.js
+```
+Fire up a web browser and go to
+```
+http://localhost:8080
+```
+__The port above may be different! Check the console for details!__
+Lastly, to access the api portion of the program, this is the url scheme:
+```
+http://localhost:8080/api?url=YOUTUBE_URL
+```
+
+Enjoy :)
---
+++ index.js
@@ -0,0 +1,111 @@
+var downloader = require('./node_modules/youtube-dl/lib/downloader.js');
+var request = require('request');
+var app = require('express')();
+
+var port = process.env.PORT || 8080;
+
+function update() {
+    return new Promise(function(resolve, reject) {
+        downloader(null, function(err, done) {
+            'use strict';
+            if (err) { return console.log(err.stack); }
+            console.log(done);
+            resolve();
+        });
+    });
+}
+
+function getInfo(url, youtubedl, callback) {
+    var options = ["-f best"];
+    youtubedl.getInfo(url, options, function(err, info) {
+        if (err) {
+            console.log('Error getting url ' + url + ': ' + err);
+            callback(null, null, err);
+            return;
+        }
+        var format = info._filename.split('.');
+        format = '.'+format[format.length-1]
+        callback(info.url, info.title, null, format);
+    });
+}
+
+function generateHTMLLink(url, title, format) {
+    return `<a href='${url.replace(/\\/g, "\\\\").replace(/'/g, "\\'")}' download='${title.replace(/\\/g, "\\\\").replace(/'/g, "\\'")+format}'>${title}</a>`;
+}
+
+(async function() {
+    var youtubedl = require('youtube-dl');
+    
+    console.log('Downloading newest version of youtube-dl');
+    await update();
+    
+    app.get('/', function(req, res) {
+        res.send(`<html>
+    <head>
+        <title>Youtube Link Extractor</title>
+    </head>
+    <body>
+        <div style="text-align: center; margin-top: 5%">
+            <h1>Youtube Link in Highest Quality</h1>
+            <h5>By Simon Cheng, special thanks to the awesome <a href="https://github.com/przemyslawpluta/node-youtube-dl">youtube-dl library</a></h5>
+            <h3>Insert link below!</h3>
+            <input id="urlField" type="text" placeholder="Youtube Link" style="width: 40%; height: 3.5%;"/>
+            <button onclick="var result = document.getElementsByTagName('iframe')[0], xhttp = new XMLHttpRequest(); result.contentDocument.body.setAttribute('style', 'text-align: center'); result.contentDocument.body.innerHTML = '<h3>Loading...</h3>'; xhttp.onerror = function(e){console.log(e); result.contentDocument.body.innerHTML = 'Could not connect to server'}; xhttp.onload = function(){result.contentDocument.body.innerHTML = xhttp.responseText}; xhttp.open('GET', '/gui?url='+window.encodeURIComponent(document.getElementById('urlField').value), true); xhttp.send();">Get link</button>
+            <br>
+            <br>
+            <iframe style='border: none; width: 70%; height: 30%'></iframe>
+            <script>document.addEventListener('keydown', function(e){
+                if(!e.ctrlKey && !e.altKey && !e.shiftKey && e.key == 'Enter') document.getElementsByTagName('button')[0].click();
+            });</script>
+        </div>
+    </body>
+</html>
+`);
+    });
+    app.get('/gui', function(req, res) {
+        if (!req.query.url) {
+            res.send('No url provided');
+            return;
+        }
+        getInfo(req.query.url, youtubedl, function(url, title, err, format) {
+            if (err) {
+                res.send(''+err);
+                return;
+            }
+            res.send(generateHTMLLink('/rerouteVid?fileURL='+encodeURIComponent(url)+'&fileName='+title+format, title, format));
+        });
+    });
+    app.get('/rerouteVid', function(req, res){
+        if(!req.query.fileURL){
+            res.send('No file url provided');
+            return;
+        }
+        res.setHeader('Content-disposition', 'attachment; filename=' + (req.query.fileName ? req.query.fileName : 'file'));
+        res.setHeader('Content-type', 'application/octet-stream');
+        request.get(req.query.fileURL).pipe(res);
+    });
+    app.get('/api', function(req, res){
+        var result = {};
+        res.setHeader('Content-Type', 'application/json');
+        if(!req.query.url){
+            result.success = false;
+            result.description = 'No url param provided'
+            res.send(JSON.stringify(result, null, 4));
+            return;
+        }
+        getInfo(req.query.url, youtubedl, function(url, title, err, format){
+            if(err){
+                result.success = false;
+                result.description = err;
+                res.send(JSON.stringify(result, null, 4));
+                return;
+            }
+            result.success = true;
+            result.title = title;
+            result.url = url;
+            result.fileName = title+format;
+            res.send(JSON.stringify(result, null, 4));
+        })
+    });
+    app.listen(port, ()=>console.log('\nApp listening on port ' + port + '!'));
+})();
